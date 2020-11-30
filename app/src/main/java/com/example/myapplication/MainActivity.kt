f0744653ea1539a7f8e7a79b7ca839993e2a83d3
dhraa98 <dhrajoshi5000@gmail.com>
2020-11-30 19:09:16 +0530

    Add map bound

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
@@ -18,10 +20,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
@@ -29,6 +28,8 @@ import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteActivity
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import java.io.IOException
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -42,8 +43,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
     var mMarker: Marker? = null
-    var mMarkerLocation: Marker? = null
-    var mMap: GoogleMap? = null
+     var mMarkerLocation: Marker? = null
+    lateinit var mMap: GoogleMap
+    var markerList: MutableList<Marker> =
+        mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -134,7 +137,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap?) {
-        mMap = googleMap
+        if (googleMap != null) {
+            mMap = googleMap
+        }
         googleMap?.setMapStyle(
             MapStyleOptions.loadRawResourceStyle(
                 this, R.raw.map_style
@@ -151,16 +156,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                         val markerIcon =
                             getMarkerIconFromDrawable(circleDrawable)
 
+                        val geocoder = Geocoder(this@MainActivity, Locale.getDefault())
 
+                        setAddress(location.latitude, location.longitude)
 
                         mMarker = googleMap.addMarker(
                             MarkerOptions()
                                 .position(currentLocation)
-                                .title("currentLocation")
+                                .title(tvSerchText.text.toString())
                                 .icon(
                                     markerIcon
                                 )
                         )
+
 
                         googleMap?.animateCamera(
                             CameraUpdateFactory.newLatLngZoom(
@@ -218,9 +226,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     data?.let {
                         val place = Autocomplete.getPlaceFromIntent(data)
                         Log.e(TAG, "Place: ${place.name}, ${place.id},${place.latLng}")
+
                         if (mMarkerLocation != null) {
                             mMarkerLocation!!.remove()
-                            mMarkerLocation = null
+                            mMarkerLocation=null
+
 
                         }
                         val circleDrawable =
@@ -239,6 +249,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                             CameraUpdateFactory.newLatLngZoom(place.latLng, 15f)
 
                         )
+                        val builder: LatLngBounds.Builder = LatLngBounds.Builder()
+
+
+                        builder.include(mMarker!!.getPosition())
+                        builder.include(mMarkerLocation!!.getPosition())
+
+                        val bounds = builder.build()
+
+                        val width = resources.displayMetrics.widthPixels
+                        val height = resources.displayMetrics.heightPixels
+                        val padding =
+                            (width * 0.10).toInt() // offset from edges of the map 10% of screen
+
+
+                        val cu: CameraUpdate =
+                            CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding)
+
+                        mMap!!.animateCamera(cu)
                         tvSerchText.text = place.name
                     }
 
@@ -261,5 +289,35 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+    private fun setAddress(latitude: Double, longitude: Double) {
+        val geocoder: Geocoder
+        var addresses: List<Address>? = null
+        geocoder = Geocoder(this, Locale.getDefault())
+        try {
+            addresses = geocoder.getFromLocation(
+                latitude,
+                longitude,
+                1
+            ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        if (addresses!!.size > 0) {
+            Log.d("max", " " + addresses[0].maxAddressLineIndex)
+            val address = addresses[0]
+                .getAddressLine(0) // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            val city = addresses[0].locality
+            val state = addresses[0].adminArea
+            val country = addresses[0].countryName
+            val postalCode = addresses[0].postalCode
+            val knownName =
+                addresses[0].featureName // Only if available else return NULL
+            addresses[0].adminArea
 
+
+            tvSerchText.text = city
+
+        }
+
+    }
 }
