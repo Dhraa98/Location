317a04024b37e4137648ed6b28e085856498c63e
dhraa98 <dhrajoshi5000@gmail.com>
2020-11-30 16:30:35 +0530

    Add autocomplete picker

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
 import android.Manifest
-
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.Transformations.map
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -18,15 +21,29 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private val TAG = "MainActivity"
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
+    lateinit var placesClient: PlacesClient
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var ivSearch: LinearLayout
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
+    var mMarker: Marker? = null
+    var mMap: GoogleMap? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,23 +53,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun initControls() {
+
+        ivSearch = findViewById(R.id.ivSearch)
+        ivSearch.setOnClickListener {
+            onSearchCalled()
+        }
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         if (!checkPermissions()) {
             requestPermissions()
         } else {
             initMap()
         }
-
     }
 
-    override fun onResume() {
-        super.onResume()
-        initMap()
-    }
 
     private fun initMap() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
+
+
     }
 
     private fun checkPermissions(): Boolean {
@@ -111,17 +132,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap?) {
-
+        mMap = googleMap
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 if (location != null) {
                     googleMap?.apply {
                         val currentLocation = LatLng(location.latitude, location.longitude)
-                        addMarker(
+                        mMarker = googleMap.addMarker(
                             MarkerOptions()
                                 .position(currentLocation)
                                 .title("currentLocation")
                         )
+
                         googleMap?.animateCamera(
                             CameraUpdateFactory.newLatLngZoom(
                                 LatLng(
@@ -139,4 +161,66 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
     }
+
+    private fun onSearchCalled() {
+        ivSearch.visibility = View.GONE
+        val apiKey = "AIzaSyDFjbzwtDSTsbCKwwjcSakbWjmM6I6nIhw"
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+        placesClient = Places.createClient(this)
+
+        val fields = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG)
+
+
+        val intent =
+            Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields).setCountry("IN")
+                .build(this)
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        Log.e(TAG, "Place: ${place.name}, ${place.id},${place.latLng}")
+                        if (mMarker != null) {
+                            mMarker!!.remove()
+                            mMarker = null
+
+                        }
+                        mMarker = mMap!!.addMarker(
+                            MarkerOptions()
+                                .position(place.latLng!!)
+                                .title("currentLocation")
+                        )
+                        mMap?.animateCamera(
+                            CameraUpdateFactory.newLatLngZoom(place.latLng, 15f)
+
+                        )
+                    }
+                    ivSearch.visibility = View.VISIBLE
+
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        Log.e(TAG, status.statusMessage!!)
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    // The user canceled the operation.
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
+
 }
